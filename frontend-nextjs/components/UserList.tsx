3d7c0c165f52393c76310f2a1a1d9a8380e47ead
Dong1311 <thanhdong1311@gmail.com>
2025-04-17 09:51:29 +0700

    fix sidebar scroll

--- frontend-nextjs/components/UserList.tsx
+++ frontend-nextjs/components/UserList.tsx
@@ -36,8 +36,8 @@ function UserList() {
   }, []);
 
   return (
-    <aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-y-auto border-r border-gray-200 dark:border-gray-700 block w-full left-0 dark:bg-black">
-      <div className="px-5">
+    <aside className="w-full lg:w-80 border-r border-gray-200 dark:border-gray-700 dark:bg-black h-full overflow-y-auto px-5">
+      <div className="h-full overflow-y-auto px-5">
         <div className="flex-col">
           <div className="text-2xl font-bold text-neutral-800 dark:text-neutral-200 py-4">
             People
@@ -55,7 +55,11 @@ function UserList() {
             No users found
           </div>
         ) : (
-          users.map((user) => currentUser?.id !== user.id ? <UserBox key={user.id} data={user} /> : '')
+          users.map((user) =>
+            currentUser?.id !== user.id ? (
+              <UserBox key={user.id} data={user} />
+            ) : null
+          )
         )}
       </div>
     </aside>
